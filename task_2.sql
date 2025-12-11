84b6880b52a5ac92e60af1780c423009fba95b1e
KwameTsakpo <godfredtsakpo@gmail.com>
2025-12-11 16:09:08 +0000

    tasl_2.sql init

--- task_2.sql
+++ task_2.sql
@@ -1,53 +1,48 @@
-
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
 
-
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
     CONSTRAINT fk_author
-        FOREIGN KEY (author_id) REFERENCES authors(author_id)
+        FOREIGN KEY (author_id) REFERENCES Authors(author_id)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
-
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
 
-
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
     CONSTRAINT fk_customer
-        FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
-
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
     CONSTRAINT fk_order
-        FOREIGN KEY (order_id) REFERENCES orders(order_id)
+        FOREIGN KEY (order_id) REFERENCES Orders(order_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     CONSTRAINT fk_book
-        FOREIGN KEY (book_id) REFERENCES books(book_id)
+        FOREIGN KEY (book_id) REFERENCES Books(book_id)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
